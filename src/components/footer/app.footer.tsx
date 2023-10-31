bf5f858f801de0d48ea953598f60b461a60aef83
ahihi <bdsthocu20@gmail.com>
2023-10-31 11:21:09 +0700

    #98.3

---
+++ src/components/footer/app.footer.tsx
@@ -0,0 +1,55 @@
+'use client'
+import { useHasMounted } from "@/utils/customHook";
+import { AppBar, Container } from "@mui/material";
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+
+
+
+const AppFooter = () => {
+
+    const hasMounted = useHasMounted();
+
+    if (!hasMounted) {
+        return (
+            <></> //fragment
+        )
+    }
+    return (
+        <AppBar
+            position="fixed"
+            sx={{
+                top: 'auto',
+                bottom: 0,
+                backgroundColor: '#f2f2f2'
+            }}
+        >
+            <Container sx={{
+                display: 'flex',
+                gap: '10px',
+            }}>
+                <AudioPlayer
+                    src="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"
+                    volume={0.5}
+                    style={{
+                        backgroundColor: '#f2f2f2',
+                        boxShadow: 'unset',
+                    }}
+                />
+                <div style={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'start',
+                    minWidth: 100
+                }}
+                >
+                    <div style={{ color: '#ccc' }}>Alex</div>
+                    <div style={{ color: 'black' }}>Who are you?</div>
+                </div>
+            </Container>
+        </AppBar>
+    )
+}
+
+export default AppFooter;
