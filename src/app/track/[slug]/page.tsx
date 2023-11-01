771c1729ddbfb96051229204d537d846d4a29b9e
ahihi <bdsthocu20@gmail.com>
2023-11-01 10:27:29 +0700

    #106

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,8 +1,14 @@
-
+'use client'
+import { useSearchParams } from 'next/navigation'
 
 
 const DetailTrackPage = (props: any) => {
     console.log(">>> check props params:", props)
+    const searchParams = useSearchParams()
+
+    const audio = searchParams.get('audio')
+    console.log(">>> check params:", audio)
+
     return (
         <div>
             Detail Track Page
