5c28abc7c3a73a628f47e699e7a9ac7aea0eafbc
ahihi <bdsthocu20@gmail.com>
2023-11-06 10:51:26 +0700

    #147.2

--- src/components/track/upload/step1.tsx
+++ src/components/track/upload/step1.tsx
@@ -1,10 +1,45 @@
 'use client'
 import { useDropzone, FileWithPath } from 'react-dropzone';
-import React from 'react';
 import './theme.css'
+import { useCallback } from 'react';
+import { styled } from '@mui/material/styles';
+import Button from '@mui/material/Button';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
+
+function InputFileUpload() {
+    return (
+        <Button
+            component="label"
+            variant="contained"
+            startIcon={<CloudUploadIcon />}
+            onClick={(e) => e.preventDefault()}
+        >
+            Upload file
+            <VisuallyHiddenInput type="file" />
+        </Button>
+    );
+}
 
 const Step1 = () => {
-    const { acceptedFiles, getRootProps, getInputProps } = useDropzone();
+    const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
+        // Do something with the files
+        console.log(">>> check acceptedFiles", acceptedFiles)
+    }, [])
+
+    const { acceptedFiles, getRootProps, getInputProps } = useDropzone({ onDrop });
 
     const files = acceptedFiles.map((file: FileWithPath) => (
         <li key={file.path}>
@@ -16,10 +51,11 @@ const Step1 = () => {
         <section className="container">
             <div {...getRootProps({ className: 'dropzone' })}>
                 <input {...getInputProps()} />
-                <p>Drag 'n' drop some files here, or click to select files</p>
+                <InputFileUpload />
+                <p>Click or Drag/Drop to upload file track</p>
             </div>
             <aside>
-                <h4>Files</h4>
+                <h4>Uploaded Files</h4>
                 <ul>{files}</ul>
             </aside>
         </section>
