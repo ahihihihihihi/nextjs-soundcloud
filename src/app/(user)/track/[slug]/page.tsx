f03d6830e57d02742164a3742497a2c818a7eed3
ahihi <bdsthocu20@gmail.com>
2023-11-09 17:32:43 +0700

    #172

--- src/app/(user)/track/[slug]/page.tsx
+++ src/app/(user)/track/[slug]/page.tsx
@@ -5,6 +5,30 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/app/(user)/api/auth/[...nextauth]/route";
 
 
+import type { Metadata, ResolvingMetadata } from 'next'
+
+type Props = {
+    params: { slug: string }
+    searchParams: { [key: string]: string | string[] | undefined }
+}
+
+export async function generateMetadata(
+    { params, searchParams }: Props,
+    parent: ResolvingMetadata
+): Promise<Metadata> {
+
+    const res = await sendRequest<IBackendRes<ITrackTop>>({
+        url: `http://localhost:8000/api/v1/tracks/${params.slug}`,
+        method: 'GET',
+    })
+
+    return {
+        title: res.data?.title,
+        description: res.data?.description,
+    }
+}
+
+
 const DetailTrackPage = async (props: any) => {
     const session = await getServerSession(authOptions)
     console.log(">>>check session DetailTrackPage", session)
