450382e7ab4ee4a7aab087a477e1ba5c0af1b4e7
ahihi <bdsthocu20@gmail.com>
2023-11-10 11:54:59 +0700

    #178.1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,13 +2,28 @@ import ThemeRegistry from '@/components/theme-registry/theme.registry';
 import NextAuthWrapper from '@/lib/next.auth.wrapper';
 import { TrackContextProvider } from '@/lib/track.wrapper';
 import { ToastProvider } from '@/utils/toast';
+import Image from 'next/image';
+import image from '../../public/flowers.jpg'
 
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeRegistry>
+        {/* <img src='flowers.jpg' alt='flowers image' /> */}
+        <Image
+          src={image}
+          alt='flowers image'
+          title='flowers'
+          sizes="100vw"
+          // Make the image display full width
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+        />
+
+        {/* <ThemeRegistry>
           <NextAuthWrapper>
             <ToastProvider>
               <TrackContextProvider>
@@ -16,7 +31,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
               </TrackContextProvider>
             </ToastProvider>
           </NextAuthWrapper>
-        </ThemeRegistry>
+        </ThemeRegistry> */}
       </body>
     </html>
   );
